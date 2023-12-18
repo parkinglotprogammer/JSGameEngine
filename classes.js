02cba5a48a52bc381d68347b7b62690064f09548
Jeremiah <programsfromparkinglots@gmail.com>
2023-12-18 05:54:18 -0800

    Cleanup
    Moved engine-like code that probably won't change between games to Game class, added hooks into main.js that the Game class will call into to keep everything clean.

--- classes.js
+++ classes.js
@@ -52,3 +52,48 @@ class Graphics {
         Graphics.vertices.push();
     }
 }
+
+class Game {
+    static {
+        Game.targetFrameRate = 30;
+        Game.targetFrameTime = 1000 / Game.targetFrameRate;
+        Game.lastTime = 0;
+        Game.quit = false;
+        Game.updateInterval;
+        Game.deltaTime;    
+    }
+    static SetTargetFrameRate(targetFrameRate) {
+        if(Game.quit)
+            return;
+        Game.targetFrameRate = targetFrameRate;
+        Game.targetFrameTime = 1000 / targetFrameRate;
+        clearInterval(Game.updateInterval);
+        Game.updateInterval = setInterval(Game.GameUpdate,Game.targetFrameTime);
+    }
+    static Start(){
+        Game.updateInterval = setInterval(Game.Update,Game.targetFrameTime);
+        requestAnimationFrame(Game.GraphicsUpdate);
+        Start();
+    }
+    static GraphicsUpdate(){
+        Graphics.Render();
+        if(!Game.quit)
+            requestAnimationFrame(Game.GraphicsUpdate);
+    }
+    static Update() {
+        // Calculate elapsed time since the last frame
+        const thisTime = performance.now();
+        Game.deltaTime = thisTime - Game.lastTime;
+        Game.lastTime = thisTime;
+        Update(Game.deltaTime);
+    }
+    static Quit() {
+        Game.quit = true;
+        Quit();
+        clearInterval(Game.updateInterval);
+    }
+}
+
+class Input {
+
+}
--- main.js
+++ main.js
@@ -1,31 +1,11 @@
-const targetFrameRate = 60;
-const targetFrameTime = 1000 / targetFrameRate;
-let lastTimestamp = 0;
-let quit = false;
-let gameUpdateInterval;
+Game.Start();
 var counter = 0;
-Start();
-
-
 function Start() {
-    gameUpdateInterval = setInterval(GameUpdate,targetFrameTime);
-    requestAnimationFrame(GraphicsUpdate);
 }
-function GraphicsUpdate(){
-    Graphics.Render();
-    if(!quit)
-        requestAnimationFrame(GraphicsUpdate);
+function Update(deltaTime) {
+    if(counter++ >= 50)
+        Game.Quit();
 }
-function GameUpdate() {
-    // Calculate elapsed time since the last frame
-    //const deltaTime = timestamp - lastTimestamp;
-    quit = counter++ >= 50;
-    // Request the next frame
-    console.log(counter);
-    if(quit)
-        Quit();
-}
+
 function Quit() {
-    clearInterval(gameUpdateInterval);
-    console.log("Game Quit");
 }
